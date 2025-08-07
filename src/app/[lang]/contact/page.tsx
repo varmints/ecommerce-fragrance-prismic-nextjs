3c7bb48e7038b312005d5c431afd938ca1801ca4
Kamil <you@example.com>
2025-08-07 15:36:13 +0200

    feat: Refactor ContactPage layout and enhance title display with RevealText component

--- src/app/[lang]/contact/page.tsx
+++ src/app/[lang]/contact/page.tsx
@@ -3,10 +3,10 @@ import { notFound } from "next/navigation";
 import { SliceZone, PrismicRichText } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
-import { Bounded } from "@/components/Bounded";
 import { FadeIn } from "@/components/FadeIn";
 import { reverseLocaleLookup } from "@/i18n";
 import { isFilled } from "@prismicio/client";
+import { RevealText } from "@/components/RevealText";
 
 type Params = Promise<{ lang: string }>;
 
@@ -21,47 +21,45 @@ export default async function ContactPage({ params }: { params: Params }) {
     .catch(() => notFound());
 
   return (
-    <Bounded className="py-12">
-      <div className="mx-auto max-w-4xl">
-        <FadeIn>
-          <div className="mb-8 text-center">
-            {isFilled.richText(page.data.title) && (
-              <div className="mx-auto mb-8">
-                <PrismicRichText
-                  field={page.data.title}
-                  components={{
-                    heading1: ({ children }) => (
-                      <h1 className="font-display text-6xl leading-none text-neutral-50 md:text-7xl lg:text-8xl">
-                        {children}
-                      </h1>
-                    ),
-                  }}
-                />
-              </div>
-            )}
-            {isFilled.richText(page.data.description) && (
-              <div className="mx-auto max-w-2xl text-lg text-balance text-gray-300">
-                <PrismicRichText
-                  field={page.data.description}
-                  components={{
-                    paragraph: ({ children }) => (
-                      <p className="mb-4 last:mb-0">{children}</p>
-                    ),
-                    strong: ({ children }) => (
-                      <strong className="font-semibold">{children}</strong>
-                    ),
-                    em: ({ children }) => (
-                      <em className="italic">{children}</em>
-                    ),
-                  }}
-                />
-              </div>
-            )}
-          </div>
-          <SliceZone slices={page.data.slices} components={components} />
+    <div className="mx-auto max-w-4xl py-40">
+      <div className="mb-8 flex flex-col items-center text-center">
+        <FadeIn vars={{ delay: 0, duration: 1.2 }} className="translate-y-8">
+          <p className="mb-4 tracking-widest uppercase">{"Contact Us"}</p>
+        </FadeIn>
+        {isFilled.richText(page.data.title) && (
+          <RevealText
+            align="center"
+            id="quiz-title"
+            field={page.data.title}
+            className="font-display text-5xl text-balance sm:text-6xl md:text-7xl"
+            duration={2}
+            as={"h1"}
+          />
+        )}
+        <FadeIn
+          className="mx-auto mb-12 max-w-2xl text-lg text-balance text-gray-300"
+          vars={{ delay: 1.3, duration: 1.3 }}
+        >
+          {isFilled.richText(page.data.description) && (
+            <PrismicRichText
+              field={page.data.description}
+              components={{
+                paragraph: ({ children }) => (
+                  <p className="mb-4 last:mb-0">{children}</p>
+                ),
+                strong: ({ children }) => (
+                  <strong className="font-semibold">{children}</strong>
+                ),
+                em: ({ children }) => <em className="italic">{children}</em>,
+              }}
+            />
+          )}
         </FadeIn>
       </div>
-    </Bounded>
+      <FadeIn className="translate-y-8" vars={{ delay: 1.5, duration: 1.3 }}>
+        <SliceZone slices={page.data.slices} components={components} />
+      </FadeIn>
+    </div>
   );
 }
 
